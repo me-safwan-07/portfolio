486224651977bfa8aefd0855229df478ef5aaf40
me-safwan-07 <mesafwan07@gmail.com>
2025-07-02 11:37:06 +0530

    Add authentication client and configuration for user sessions

---
+++ app/lib/auth.ts
@@ -0,0 +1,37 @@
+import { db } from '@tszhong0411/db'
+import { env } from '@tszhong0411/env'
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { headers } from 'next/headers'
+
+import { getBaseUrl } from '@/utils/get-base-url'
+
+export const auth = betterAuth({
+  baseURL: getBaseUrl(),
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    usePlural: true
+  }),
+  trustedOrigins: [getBaseUrl()],
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
+    },
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET
+    }
+  },
+  user: {
+    additionalFields: {
+      role: { type: 'string', required: true, input: false, defaultValue: 'user' }
+    }
+  }
+})
+
+export const getSession = async () => {
+  return await auth.api.getSession({
+    headers: await headers()
+  })
+}
